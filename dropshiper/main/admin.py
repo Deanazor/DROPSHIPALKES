8cbf4b8e9afb3b4059d478e8208691af7c3d4e4f
Deanazor <arief.p.a@mail.ugm.ac.id>
2020-11-27 20:04:12 +0700

    Added new category

--- dropshiper/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Konsumen
-# Register your models here.
-
-admin.site.register(Konsumen)
--- dropshiper/main/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class KonsumenForm(forms.Form):
-    nama = forms.CharField(max_length=200)
-    saldo = forms.IntegerField(initial=0) 
-    kontak = forms.CharField(max_length=200)
